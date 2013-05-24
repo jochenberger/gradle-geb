27559025303c0dd40a7b62ce103c0c5eb40961e6
Jochen Berger <fooberger@gmail.com>
2013-05-24 09:29:52 +0200

    version 0.0.13-SNAPSHOT: * extract versions and drivers into an extension * update default versions

--- src/main/groovy/com/github/jochenberger/gradlegeb/GebPlugin.groovy
+++ src/main/groovy/com/github/jochenberger/gradlegeb/GebPlugin.groovy
@@ -10,80 +10,73 @@ import org.gradle.api.tasks.testing.Test
 
 class GebPlugin implements Plugin<Project>{
 
-	def gebVersion = "0.9.0"
-	def seleniumVersion = "2.32.0"
-	def spockVersion = "0.7-groovy-2.0"
-	def groovyVersion = "2.1.2"
+  @Override
+  public void apply(Project p) {
 
-	def drivers = ["firefox", "chrome"]
+    p.apply plugin: 'jetty'
+    p.apply plugin: 'groovy'
+    p.extensions.create('geb', GebPluginExtension)
+    p.afterEvaluate{
+      p.dependencies.add("testCompile", "org.gebish:geb-spock:$p.geb.gebVersion")
+      p.dependencies.add("testCompile", "org.codehaus.groovy:groovy-all:$p.geb.groovyVersion")
 
+      p.dependencies.add("testCompile", "org.spockframework:spock-core:$p.geb.spockVersion")
+      p.geb.drivers.each {
+        p.dependencies.add("testCompile", "org.seleniumhq.selenium:selenium-$it-driver:$p.geb.seleniumVersion")
+      }
+      p.dependencies.add("testCompile", "org.seleniumhq.selenium:selenium-support:$p.geb.seleniumVersion")
+    }
 
-	@Override
-	public void apply(Project p) {
-		p.apply plugin: 'jetty'
-		p.apply plugin: 'groovy'
+    p.task([type: JettyRun, description: "Starts a Jetty server", group: "Check"], "jettyTest")
+    p.tasks.test.doFirst {
+      File spockConfig = File.createTempFile("spockconfig", ".groovy", temporaryDir)
+      spockConfig.text = GebPlugin.class.getResourceAsStream("SpockConfig.groovy").text
+      systemProperty "spock.configuration", spockConfig.absolutePath
+    }
 
-		p.dependencies.add("testCompile", "org.gebish:geb-spock:$gebVersion")
-		p.dependencies.add("testCompile", "org.codehaus.groovy:groovy-all:$groovyVersion")
+    Closure removeEmptyResults = {
+      logger.debug "Analyzing results in $testResultsDir"
+      testResultsDir.eachFileMatch FileType.FILES, ~/.*\.xml/, {
+        logger.debug "Analyzing $it"
+        int numberOfTests = new XmlSlurper().parse(it).@tests.text() as int
+        if (numberOfTests == 0){
+          logger.debug "$it is empty"
+          it.delete()
+        }else{
+          logger.debug "$it has $numberOfTests test results"
+        }
+      }
+    }
+    p.afterEvaluate{
+      p.geb.drivers.each { String d ->
 
-		p.dependencies.add("testCompile", "org.spockframework:spock-core:$spockVersion")
-		drivers.each {
-			p.dependencies.add("testCompile", "org.seleniumhq.selenium:selenium-$it-driver:$seleniumVersion")
-		}
-		p.dependencies.add("testCompile", "org.seleniumhq.selenium:selenium-support:$seleniumVersion")
+        Task t = p.task([type: Test, description: "Run Selenium tests with $d", group: "Test"], "${d}Test").doFirst {
+          File spockConfig = File.createTempFile("spockconfig", ".groovy", temporaryDir)
+          spockConfig.text = GebPlugin.class.getResourceAsStream("SpockConfigUI.groovy").text
+          systemProperty "spock.configuration", spockConfig.absolutePath
+          systemProperty "geb.env", d
+          systemProperty "geb.build.baseUrl", "http://localhost:${p.tasks.jettyTest.httpPort}/${p.tasks.jettyTest.contextPath}/"
+          testResultsDir p.file("${p.buildDir}/${d}test-results")
+          testReportDir p.file("${p.reporting.baseDir}/${d}tests")
+        }
 
+        t.doFirst {
+          p.tasks.jettyTest.daemon = true;
+          p.tasks.jettyTest.execute();
+        }
 
-		p.task([type: JettyRun, description: "Starts a Jetty server", group: "Check"], "jettyTest")
-		p.tasks.test.doFirst {
-			File spockConfig = File.createTempFile("spockconfig", ".groovy", temporaryDir)
-			spockConfig.text = GebPlugin.class.getResourceAsStream("SpockConfig.groovy").text
-			systemProperty "spock.configuration", spockConfig.absolutePath
-		}
+        t.doLast removeEmptyResults
+        //t.inputs.files p.tasks.test.inputs.files
+        //t.outputs.files p.tasks.test.outputs.files
 
-		Closure removeEmptyResults = {
-			logger.debug "Analyzing results in $testResultsDir"
-			testResultsDir.eachFileMatch FileType.FILES, ~/.*\.xml/, {
-				logger.debug "Analyzing $it"
-				int numberOfTests = new XmlSlurper().parse(it).@tests.text() as int
-				if (numberOfTests == 0){
-					logger.debug "$it is empty"
-					it.delete()
-				}else{
-					logger.debug "$it has $numberOfTests test results"
-				}
-			}
-		}
+        p.tasks.test.dependsOn(t)
+      }
+    }
+    p.tasks.test.doLast removeEmptyResults
+    // TODO shut-down jetty after tests
 
-		drivers.each { String d ->
-
-			Task t = p.task([type: Test, description: "Run Selenium tests with $d", group: "Test"], "${d}Test").doFirst {
-				File spockConfig = File.createTempFile("spockconfig", ".groovy", temporaryDir)
-				spockConfig.text = GebPlugin.class.getResourceAsStream("SpockConfigUI.groovy").text
-				systemProperty "spock.configuration", spockConfig.absolutePath
-				systemProperty "geb.env", d
-				systemProperty "geb.build.baseUrl", "http://localhost:${p.tasks.jettyTest.httpPort}/${p.tasks.jettyTest.contextPath}/"
-				testResultsDir p.file("${p.buildDir}/${d}test-results")
-				testReportDir p.file("${p.reporting.baseDir}/${d}tests")
-			}
-
-			t.doFirst {
-				p.tasks.jettyTest.daemon = true;
-				p.tasks.jettyTest.execute();
-			}
-
-			t.doLast removeEmptyResults
-			//t.inputs.files p.tasks.test.inputs.files
-			//t.outputs.files p.tasks.test.outputs.files
-
-			if (d == 'firefox'){
-				p.tasks.test.dependsOn(t)
-			}
-		}
-		p.tasks.test.doLast removeEmptyResults
-		// TODO shut-down jetty after tests
-
-		//		p.tasks.test.doLast {
-		//			p.tasks.jettyStop.execute()
-		//		}
-	}
+    //		p.tasks.test.doLast {
+    //			p.tasks.jettyStop.execute()
+    //		}
+  }
 }
---
+++ src/main/groovy/com/github/jochenberger/gradlegeb/GebPluginExtension.groovy
@@ -0,0 +1,10 @@
+package com.github.jochenberger.gradlegeb
+
+class GebPluginExtension {
+  def gebVersion = "0.9.0"
+  def seleniumVersion = "2.33.0"
+  def spockVersion = "0.7-groovy-2.0"
+  def groovyVersion = "2.1.3"
+
+  def drivers = ["firefox"]
+}
